4224d9e5697b908b332d5f1829d0efd6d6d70305
choilettuce <tkdgnstkdcn@naver.com>
2024-07-14 18:58:26 +0900

    create : api

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-const http = require("http").Server(app);
+// const port = /*process.env.PORT ||*/ 8000;
+// const https = require("https").Server(app);
 
+// app.listen(port, function(){
+//   console.log("서버시작!")
+// })
+
+const Server = app.listen(8000, () => {
+  console.log(`서버시작 : localhost 8000`);
+})
+
+// cors
 const cors = require("cors");
 const corsQptions = {
   origin: "http://localhost:8000",
   Credential: true,
 };
-
 app.use(cors(corsQptions));
+  
+// /* env */
+// require("dotenv").config();
